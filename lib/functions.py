a454ada299e1d3142e4c5b0696790f8f63d881cb
Maxi-arch <mxmu025@gmail.com>
2024-11-25 13:12:42 +0300

    1stcommit

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
-def greet_programmer():
-    pass
+def greet_programmer(param):
+    print("Hello, programmer!")
+    return param 
 
 def greet(name):
-    pass
+    print(f'hello {name}!')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return num1 +num2
 
 def halve(number):
-    pass
+    return number/2
